84161873101768789d01ec2ebb7fa70cf5c570f7
ian <me@iany.me>
2022-07-26 20:07:42 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Export Discord Channel Messages as Markdown
+
+## Usage
+
+### Get User Token
+
+- Login Discord App in Chrome
+- Open Developer Tool. Search `api` in the Network tab. Find any API request having the request header `Authorization`, copy the header value.
+- Export the header value in the last step as the environment variable `DISCORD_USER_TOKEN`.
+
+### Copy Message Link
+
+Right click any message and copy the link, which looks like `https://discord.com/channels/123456789518408213/123456789047021571/1234567896626376714`.
+
+### Run the Script
+
+```
+./discord-export.py -h
+usage: discord-export.py [-h] [--context {around,after,before}] [--limit N] URL
+
+Export Discord Messages
+
+positional arguments:
+  URL                   Discord Message URL
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --context {around,after,before}
+                        How to fetch messages relative to the specified message
+  --limit N             Number of messages to export
+```
---
+++ discord-export.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import requests
+import re
+import textwrap
+from datetime import datetime
+from urllib.parse import urlparse
+
+ESCAPE_RE = re.compile(r'[\[\]]')
+SCRUB_RE = re.compile(r'[\u2028]')
+MENTION_RE = re.compile(r'<@([\d]+)>')
+
+
+def escape_text(text):
+    return re.sub(ESCAPE_RE, r'\\\g<0>', text)
+
+
+def scrub_text(text):
+    return re.sub(SCRUB_RE, '', text)
+
+
+def indent_text(text, prefix):
+    if text != '':
+        return textwrap.indent(text, prefix)
+
+    return ''
+
+
+def format_one_embed(embed):
+    lines = []
+
+    if 'title' in embed:
+        if 'url' in embed:
+            title = f'[**{escape_text(embed["title"])}**]({embed["url"]})'
+        else:
+            title = f'**{escape_text(embed["title"])}**'
+        lines.append(f'    > {title}')
+    elif 'url' in embed:
+        lines.append(f'    > <{embed["url"]}>')
+
+    if 'description' in embed and embed['description'].strip() != '':
+        if len(lines) > 0:
+            lines.append('    >')
+        lines.append(indent_text(scrub_text(embed['description']), "    > "))
+    if 'thumbnail' in embed:
+        lines.append("    >")
+        lines.append(f'    > ![]({embed["thumbnail"]})')
+    return "\n".join(lines)
+
+
+def format_embeds(embeds):
+    if len(embeds) > 0:
+        return "\n" + "\n\n".join(map(format_one_embed, embeds)) + "\n"
+
+    return ""
+
+
+def format_attachments(attachments):
+    lines = []
+
+    for attachment in attachments:
+        if attachment.get('width') is not None:
+            lines.append(f'   ![]({attachment["proxy_url"]})')
+        else:
+            lines.append(
+                f'   [{attachment["name"]}]({attachment["proxy_url"]})')
+
+    if len(lines) > 0:
+        lines.append("")
+
+    return "\n".join(lines)
+
+
+def handle_user_mentions(text, mentions):
+    def repl(match):
+        if match.group(1) in mentions:
+            return f'@{mentions[match.group(1)]}'
+        else:
+            return match.group(0)
+    return re.sub(MENTION_RE, repl, text)
+
+
+def format_date(isodate):
+    return datetime.fromisoformat(isodate).strftime('%c')
+
+
+def format_one_message(message):
+    indexed_mentions = {}
+    for mention in message['mentions']:
+        indexed_mentions[mention['id']] = mention['username']
+    embeds = format_embeds(message['embeds'])
+    attachments = format_attachments(message['attachments'])
+    return "\n".join([
+        f'- **{message["author"]["username"]}** ({format_date(message["timestamp"])}): ',
+        "",
+        indent_text(scrub_text(handle_user_mentions(
+            message['content'], indexed_mentions)), "    "),
+        embeds,
+        attachments
+    ])
+
+
+parser = argparse.ArgumentParser(
+    description='Export Discord Messages')
+parser.add_argument('url', metavar='URL',
+                    help='Discord Message URL')
+parser.add_argument('--context', choices=['around', 'after', 'before'], default='around',
+                    help='How to fetch messages relative to the specified message')
+parser.add_argument('--limit', metavar='N', type=int, default=11, choices=range(1, 101),
+                    help='Number of messages to export')
+
+args = parser.parse_args()
+url = urlparse(args.url)
+guild_id, channel_id, message_id = url.path.split('/')[2:]
+
+messages_json = requests.get(f'https://discordapp.com/api/v9/channels/{channel_id}/messages?limit={args.limit}&{args.context}={message_id}', headers={
+    'Authorization': os.environ['DISCORD_USER_TOKEN']
+}).json()
+if 'code' in messages_json:
+    print(messages_json, file=sys.stderr)
+    sys.exit(127)
+
+messages = list(map(format_one_message, reversed(messages_json)))
+
+print(f'[※ Open Thread in Discord]({args.url})\n\n' + "\n".join(messages))
